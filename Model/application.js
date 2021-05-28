f7466ee52572704d2bf1cded912657b28913ab79
oladele <46722879+oladeledee@users.noreply.github.com>
2021-05-28 17:52:23 +0100

    Update application.js

--- Model/application.js
+++ Model/application.js
@@ -1,23 +1,20 @@
 const  mongoose = require ( 'mongoose');
-const {Student}= require('../Model/student')
 
 const applicationSchema = new mongoose.Schema({
     student:
-    
-    {  type:mongoose.Schema.Types.ObjectId,
+           {  type:mongoose.Schema.Types.ObjectId,
         ref:"Student",    
-    } ,
+           } ,
     program: {
-        type:String,
-        required:true
-    },
+          type:String,
+          required:true
+         },
     semester:{
         type:String,
         required:true
-    },  
+        },  
 },
-{timestamps:true}
+     {timestamps:true}
 )
 
-const Application = mongoose.model('application', applicationSchema);
-exports.Application=  Application;
+module.exports = mongoose.model('application', applicationSchema);
