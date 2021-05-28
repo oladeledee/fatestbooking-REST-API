ebf0d8994ec8b9ac4c927618e9f1d489165081cf
oladele <46722879+oladeledee@users.noreply.github.com>
2021-05-28 20:41:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express= require ('express');
 const cors = require('cors');
 const mongoose = require('mongoose');               
 const bodyParser = require('body-parser');
+const errorMiddlewares= require('./middlewares/errorMiddleware');
 
 const app = express();
 
 // middlewares
+errorMiddlewares(app);
 app.use(cors());
 app.use(bodyParser.urlencoded(
     {extended:false}
