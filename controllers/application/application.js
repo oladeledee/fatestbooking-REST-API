bf48efc1bc572940ef4d3e7988388ce5c7ec8c53
oladele <46722879+oladeledee@users.noreply.github.com>
2021-05-29 15:42:37 +0100

    Update application.js

--- controllers/application/application.js
+++ controllers/application/application.js
@@ -1,88 +1,92 @@
-const {Student}= require('../../model/student');
+const {Application}= require('../../model/application');
+const {Student} = require('../../model/student');
 
 module.exports = {
-
-create: async(req,res)=>{
-  try {
-    const {firstName,lastName,userName,email,cgpa}=req.body;
-     const student = await Student.create({
-        firstName,
-        lastName,
-        userName,
-        email,
-        cgpa
-     })
-     await student.save();
-     return res.send(student);
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).send({message:'internal server error'});
+  create: async(req,res)=>{
+try {
+  let student_Id=  req.params.id;
+    const student= await Student.findOne(student_Id).populate('student');
+    console.log(student);
+    if(student){ 
+      const{program,semester}= req.body;
+      const applications = new Application({
+      program,
+      semester,
+      student:student._id
+     });
+     
+     console.log(applications);
+       await applications.save();
+     res.send({applications});
   }
-    
-},
+  else{
+    res.status(404).send({ message: '  Student id isnt valid' });  }
+} 
 
-findAll: async(req,res) =>{
-  try {
-    const student = await Student.find()
-    if(student){
-      return res.send(student);
+ catch (error) {
+  console.error(error.message)
+  res.status(500).send({message:'internal server error'});
+} 
+     }   ,
+
+find: async(req,res) =>{
+  try{
+    const applications=await Application.find().populate('student');
+    if(applications){   
+       res.send(applications);
     }
-    else {
-      res.status(404).send({ message: 'student Not Found' });
-       }
-  } catch (error) {
+    else{
+        res.status(404).send({ message: ' No Student application Details  Found' });
+    }
+  }
+   catch(error) {
     console.error(error.message)
     res.status(500).send({message:'internal server error'});
-  }
-    
+    //throw  error
+      }
+  
 },
-
 
 findById : async(req,res)=>{
   try {
-    const {id} = req.params;
+    const id = req.params._id;
+    const application = await Application.findById(id).populate('student');
+          if (application) {
+            res.send(application.student);
+          } else {
+            res.status(404).send({ message: 'Application with the given id Not Found' });
+          }
+  } catch (error) {
+      console.error(error.message)
+      res.status(500).send({message:'internal server error'});
+  }    
+},
 
-    const student = await Student.findById(id);
-    if(student){
-      res.send(student);
-    }
-    else {
-      res.status(404).send({ message: 'student with the given Not Found' });
+update:(async (req, res) => {
+  try {
+    const id = req.params.id;
+    const application = await Application.findById(id);
+    if (application) {
+        const applications = await Application.update({
+            student:student.id,
+        program:req.body.program ,
+       semester:req.body.semester,
+         });
+      const updatedApplication = await applications.save();
+      res.send({ message: 'Application Details Updated', application: updatedApplication });
+    } else {
+      res.status(404).send({ message: 'Application Details Found' });
     }
   } catch (error) {
     console.error(error.message)
     res.status(500).send({message:'internal server error'});
   }
    
-},
-
-update:(async (req, res) => {
-  try {
-    const {id} = req.params;
-    const student = await Student.findById(id);
-    if (student) {    
-        const student = await Student.update({
-            firstName:req.body.firstName,
-            lastName:req.body.lastName,
-            email:req.body.email,
-            cgpa:req.body.cgpa
-         });
-     
-      const updatedstudent = await student.save();
-      res.send({ message: 'student Details Updated', student: updatedstudent });
-    } else {
-      res.status(404).send({ message: 'student Details Found' });
-    }
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).send({message:'internal server error'});
-  }
-    
 }),
 
 delete: (async(req,res)=>{
   try {
-    const {id} = req.params;
+    const id = req.params.id;
     const student = await Student.findById(id);
     if (student) {
     
@@ -91,12 +95,12 @@ delete: (async(req,res)=>{
       } else {
         res.status(404).send({ message: 'student details Not Found' });
       }
-
+  
   } catch (error) {
     console.error(error.message)
     res.status(500).send({message:'internal server error'});
   }
-        }),   
+   }),
 }
 
 
